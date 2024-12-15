848602abdc0dedb27bc24d6995037ce4d34b3c41
thornbeam <thornbeam@posteo.eu>
2024-12-15 14:36:38 +0100

    nvim: fix tab mapping on nvim cmd

--- .config/nvim/lua/config/cmp.lua
+++ .config/nvim/lua/config/cmp.lua
@@ -1,7 +1,14 @@
 -- Set up nvim-cmp.
 local cmp = require("cmp")
-local t = function(str)
-	return vim.api.nvim_replace_termcodes(str, true, true, true)
+
+--local t = function(str)
+--	return vim.api.nvim_replace_termcodes(str, true, true, true)
+--end
+
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup({
@@ -19,63 +26,60 @@ cmp.setup({
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
 	},
-	mapping = cmp.mapping.preset.insert({
-		["<Tab>"] = cmp.mapping({
-			c = function()
-				if cmp.visible() then
-					cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-				else
-					cmp.complete()
-				end
-			end,
-			i = function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-				elseif vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-					vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), "m", true)
-				else
-					fallback()
-				end
-			end,
-			s = function(fallback)
-				if vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-					vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), "m", true)
-				else
-					fallback()
-				end
-			end,
+	mapping = {
+
+		-- ... Your other configuration ...
+
+		["<C-Space>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Insert,
+			select = true,
 		}),
-		["<S-Tab>"] = cmp.mapping({
-			c = function()
-				if cmp.visible() then
-					cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-				else
+
+		["<Tab>"] = function(fallback)
+			if not cmp.select_next_item() then
+				if vim.bo.buftype ~= "prompt" and has_words_before() then
 					cmp.complete()
-				end
-			end,
-			i = function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-				elseif vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
-					return vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_backward)"), "m", true)
 				else
 					fallback()
 				end
-			end,
-			s = function(fallback)
-				if vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
-					return vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_backward)"), "m", true)
+			end
+		end,
+
+		["<S-Tab>"] = function(fallback)
+			if not cmp.select_prev_item() then
+				if vim.bo.buftype ~= "prompt" and has_words_before() then
+					cmp.complete()
 				else
 					fallback()
 				end
-			end,
-		}),
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
+			end
+		end,
+	},
+
+	snippet = {
+		-- We recommend using *actual* snippet engine.
+		-- It's a simple implementation so it might not work in some of the cases.
+		expand = function(args)
+			unpack = unpack or table.unpack
+			local line_num, col = unpack(vim.api.nvim_win_get_cursor(0))
+			local line_text = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, true)[1]
+			local indent = string.match(line_text, "^%s*")
+			local replace = vim.split(args.body, "\n", true)
+			local surround = string.match(line_text, "%S.*") or ""
+			local surround_end = surround:sub(col)
+
+			replace[1] = surround:sub(0, col - 1) .. replace[1]
+			replace[#replace] = replace[#replace] .. (#surround_end > 1 and " " or "") .. surround_end
+			if indent ~= "" then
+				for i, line in ipairs(replace) do
+					replace[i] = indent .. line
+				end
+			end
+
+			vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, true, replace)
+		end,
+	},
+
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
 		-- { name = 'vsnip' }, -- For vsnip users.
